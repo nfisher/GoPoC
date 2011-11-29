32c2d49ddb8409afe2ba98d5f8221251502130c2
Nathan Fisher <nfisher@junctionbox.ca>
2011-11-30 09:29:13 +1100

    Goagent module now uses parameterised config.

--- src/puppet/modules/goagent/manifests/init.pp
+++ src/puppet/modules/goagent/manifests/init.pp
@@ -1,11 +1,19 @@
-class goagent($conf_file) {
-	file { 'goagent':
-		path => '/etc/defaults/go-agent',
+class goagent($go_server = "10.0.0.2") {
+	package {"go-agent":
+		provider => rpm,
+		ensure => present,
+		source => "http://download01.thoughtworks.com/go/2.4.0/ga/go-agent-2.4.0-14481.noarch.rpm",
+		require => Class["jdk"],
+	}
+
+	file { '/etc/default/go-agent':
 		ensure => file,
-		group => root,
-		owner => root,
+		mode => '644',
+		group => go,
+		owner => go,
+		require => Package["go-agent"],
 		notify => Service['go-agent'],
-		source => "puppet:///modules/goagent/$conf_file",
+		content => template("goagent/go-agent.erb"),
 	}
 
 	service { 'go-agent':
